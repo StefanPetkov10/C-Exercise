aacb1e291732008f50b361e6fa39e53f4584a728
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-06-06 10:56:05 +0300

    Add C tasks

---
+++ Laboratory exercise/Exercise 6/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "task1.h"
+#include "task2.h"
+#include "task4.h"
+#include "task4.1.h"
+#include "task5.h"
+#include "task6.h"
+
+
+
+int main()
+{
+    task5();
+}
