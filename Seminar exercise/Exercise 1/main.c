0fd42135a48a63d0788b9422ae60b33931707153
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-06-11 00:46:58 +0300

    Add C tasks

---
+++ Seminar exercise/Exercise 1/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "zad1.h"
+#include "zad2.h"
+#include "zad3.h"
+#include "zad4.h"
+#include "zad5.h"
+#include "zad6.h"
+#include "zad7.h"
+#include "zad8.h"
+#include "zad9.h"
+#include "zad10.h"
+#include "zad11.h"
+
+
+
+int main()
+{
+    zad4();
+
+    return 0;
+}
