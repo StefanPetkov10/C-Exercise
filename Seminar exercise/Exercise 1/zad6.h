56963f2ea0529ed688729b3d9d1e748230105251
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-06-12 20:51:48 +0300

    Add C tasks

---
+++ Seminar exercise/Exercise 1/zad6.h
@@ -0,0 +1,15 @@
+#ifndef ZAD6_H_INCLUDED
+#define ZAD6_H_INCLUDED
+
+void zad6() {
+
+    double c, f;
+    printf("Enter C: ");
+    scanf("%lf", &c);
+
+    f = c * 33.8;
+    printf("In F is: %.2f: ", f);
+
+}
+
+#endif // ZAD6_H_INCLUDED
