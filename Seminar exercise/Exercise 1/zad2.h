9cfdfa6ec12865c31bdb3485c83c53d87b67adc0
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-06-12 20:50:31 +0300

    Add C tasks

---
+++ Seminar exercise/Exercise 1/zad2.h
@@ -0,0 +1,16 @@
+#ifndef ZAD2_H_INCLUDED
+#define ZAD2_H_INCLUDED
+
+void zad2(){
+
+    for(int i = 1; i <= 10; i++){
+        for(int j = i; j>=1; j--){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+}
+
+
+#endif // ZAD2_H_INCLUDED
