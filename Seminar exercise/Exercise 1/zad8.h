aeb39c2650121fc86c65efb64cd7ce56b6d1159b
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-06-13 10:31:27 +0300

    Add C tasks

---
+++ Seminar exercise/Exercise 1/zad8.h
@@ -0,0 +1,16 @@
+#ifndef ZAD8_H_INCLUDED
+#define ZAD8_H_INCLUDED
+
+void zad8(){
+    double lv, dolar;
+    printf("Enter a leva: ");
+    scanf("%lf", &lv);
+
+    dolar = lv * 1.86;
+    printf("In dolars is: %.2f: ", dolar);
+
+}
+
+
+
+#endif // ZAD8_H_INCLUDED
