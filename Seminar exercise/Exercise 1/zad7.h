2870b7b1977f9cc16fe9febbc2e4e7d6822aaf50
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-06-13 10:31:09 +0300

    Add C tasks

---
+++ Seminar exercise/Exercise 1/zad7.h
@@ -0,0 +1,17 @@
+#ifndef ZAD7_H_INCLUDED
+#define ZAD7_H_INCLUDED
+
+void zad7() {
+
+    double r, rad;
+    printf("Enter radius: ");
+    scanf("%lf", &r);
+
+    rad = r * 3.14 / 180 ;
+    printf("In rad is: %.2f: ", rad);
+
+}
+
+
+
+#endif // ZAD7_H_INCLUDED
