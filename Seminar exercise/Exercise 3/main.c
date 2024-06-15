4401d1516baca0da77221316a2cbe04fdbd8c628
StefanPetkov10 <106475521+StefanPetkov10@users.noreply.github.com>
2024-06-15 10:40:52 +0300

    Add C tasks

---
+++ Seminar exercise/Exercise 3/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "task1.h"
+#include "task2.h"
+#include "task3.h"
+#include "task4.h"
+#include "task5.h"
+#include "task6.h"
+#include "task7.h"
+#include "task8.h"
+#include "task9.h"
+#include "task10.h"
+#include "task11.h"
+
+int main(){
+
+    task11();
+}
